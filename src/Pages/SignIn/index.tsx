f81dd760ffaa57dd817c2de3cddb1bb0e458d033
Filipe Secherini <filipe.secherii@gmail.com>
2023-07-17 19:48:20 -0300

    Adicionado pagina de cadastro

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -1,5 +1,23 @@
+import logo from '../../assets/images/logo.png'
+import * as C from './style';
+
 export const SignIn = () => {
      return (
-          <h1>Página de cadastro</h1>
+          <C.singContainer>
+               <div>
+                    <img src={logo} alt='Logo Shopping'/>
+               </div>
+               <div>
+                    <h1>Cadastro de Funcionarios</h1>
+                    <form action="login" method="post">
+                         <input type="text" name="nome" placeholder="Nome completo"></input>
+                         <input type="text" name="cpf" placeholder="CPF"></input>
+                         <input type="email" name="email" placeholder="E-mail"></input>
+                         <input type="password" name="password" placeholder="Senha"></input>
+                         <input type="text" name="cargo" placeholder="Cargo"></input>
+                         <input className='botao' type="submit" value="Cadastro"></input>
+                    </form>
+               </div>
+          </C.singContainer>     
      );
 }
